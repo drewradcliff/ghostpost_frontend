ab653b596c1642808565c41858190617e226a2b7
Drew Radcliff <sirampr@gmail.com>
2020-10-01 11:27:07 -0400

    Add basic react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
 
-function App() {
+export default function App() {
   return (
-    <div>Hello world</div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/top">Top</Link>
+            </li>
+            <li>
+              <Link to="/boasts">Boasts</Link>
+            </li>
+            <li>
+              <Link to="/roasts">Roasts</Link>
+            </li>
+            <li>
+              <Link to="/post">Submit Posts</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/top">
+            <h1>Top</h1>
+          </Route>
+          <Route path="/boasts">
+            <h1>Boasts</h1>
+          </Route>
+          <Route path="/roasts">
+            <h1>Roasts</h1>
+          </Route>
+          <Route path="/post">
+            <h1>Submit Post</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
