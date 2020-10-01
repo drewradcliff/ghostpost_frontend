46ba2af2efbb2b3f0218070dc01d3880540dc2ee
Drew Radcliff <sirampr@gmail.com>
2020-10-01 12:31:45 -0400

    Create post component with basic styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,19 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
+import Post from "./Post"
 
 export default function Home() {
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/posts/")
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  }, []);
+  
   return (
-    <h1>home</h1>
+    <>
+      {posts.map(post => (
+        <Post key={post.id} post={post} />
+      ))}
+    </>
   )
 }
---
+++ src/components/Post.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Post.css";
+
+export default function Post({ post }) {
+  const d = new Date(post.submission_date);
+
+  return (
+    <div className="post">
+      <div className="score-div">
+        <button>⬆</button>
+        <div className="score">{post.score}</div>
+        <button>⬇</button>
+      </div>
+      <div>
+        <ul>
+          <p>{post.post_text}</p>
+          <p>{d.toDateString()}</p>
+        </ul>
+      </div>
+    </div>
+  );
+}
